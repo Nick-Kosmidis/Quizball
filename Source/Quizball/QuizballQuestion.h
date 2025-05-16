662d3d0bd7bbc89329367417f5a74f8a845fa773
Nick-Kosmidis <kosmidisnikos123@gmail.com>
2025-05-16 18:44:47 +0300

    Revert "Big updated on whosmissing and IDs"
    This reverts commit e99e7a6d25713fe86cb20e3fcdad64da03e76958.

--- Source/Quizball/QuizballQuestion.h
+++ Source/Quizball/QuizballQuestion.h
@@ -98,18 +98,6 @@ struct FQuizballQuestionData
 	}
 };
 
-USTRUCT(BlueprintType)
-struct FIDTeamsAndPeriods 
-{ 
-	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString Teams; 
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString Periods; 
-};
-
 
 UCLASS()
 class QUIZBALL_API AQuizballQuestion : public AActor
@@ -168,16 +156,12 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SelectRandomQuestions();
 
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BLUEPRINTCALLABLE)
 	FString GetPlayerPosition(const FString& player);
-
-	UFUNCTION(BlueprintCallable)
-	FIDTeamsAndPeriods GetTeamsAndPeriods(const FString& question);
 	
 	int32 SetMaxCharacters(const EQuestionCategory& category);
 	char SetSeperateSymbol(const EQuestionCategory& category);
 	void RemoveCharacter(FString& question, const char& seperateCharacter);
-	 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
